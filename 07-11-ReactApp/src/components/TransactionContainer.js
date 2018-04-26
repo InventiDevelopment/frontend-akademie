50f69547c19f13d327b8ceb3557dab6df262d6c6
Martina Kubištová <martina.kubistova@centrum.cz>
2018-04-26 19:37:56 +0200

    app

---
+++ 07-11-ReactApp/src/components/TransactionContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Transaction from './Transaction';
+
+export default class TransactionContainer extends Component {
+  state = { expanded: false }
+
+  clickHandler = () => {
+    this.setState({ expanded: !this.state.expanded })
+  }
+
+  render() {
+    return (
+      <Transaction data={this.props.data}
+        expanded={this.state.expanded}
+        toggleExpanded={this.clickHandler}
+        deleteTransaction={this.props.deleteTransaction} />
+    )
+  }
+}
---
+++ 07-11-ReactApp/src/components/TransactionList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Modal from 'react-modal';
+import transactions from '../data/transactions';
+import TransactionContainer from './TransactionContainer';
+
+export default class TransactionList extends React.Component {
+  state = {
+    stateTransactions: [],
+    modalOpen: false
+   }
+
+  componentDidMount() {
+    this.setState({ stateTransactions: transactions })
+  }
+
+  deleteTransaction = (transaction) => {
+    const newTransactions = this.state.stateTransactions.filter(
+      object => JSON.stringify(transaction) !== JSON.stringify(object)
+    )
+    this.setState({ stateTransactions: newTransactions })
+  }
+
+  closeModal = () => {
+    this.setState({ modalOpen: false })
+  }
+
+  openModal = () => {
+    this.setState({ modalOpen: true })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <ul>
+          {
+            this.state.stateTransactions.map((transaction) =>
+              <TransactionContainer data={transaction} deleteTransaction={this.deleteTransaction} />
+            )
+          }
+        </ul>
+        <button onClick={this.openModal}>Open modal</button>
+        <Modal isOpen={this.state.modalOpen} onRequestClose={this.closeModal}>
+          <input />
+          <input />
+        </Modal>
+      </React.Fragment>
+    )
+  }
+}
