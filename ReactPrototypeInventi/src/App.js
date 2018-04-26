4cbdcba5367395b7d5538b0184b79a760f972feb
martin.machacek <martin.machacek@inventi.cz>
2018-04-26 19:28:18 +0200

    Add router

--- ReactPrototypeInventi/src/App.js
+++ ReactPrototypeInventi/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import TransactionList from './components/TransactionList';
+import Overview from './components/Overview';
 
 class App extends Component {
   render() {
     return (
-      <div className="page">
-        <header className="App-header">
-          <h1>Welcome to React</h1>
-        </header>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <TransactionList />
-      </div>
+      <Router>
+        <div className="page">
+          <header className="App-header">
+            <h1>Welcome to React</h1>
+          </header>
+          <p>
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+            <Link to="/">To homepage</Link>
+            <Link to="/overview">To overview</Link>
+          </p>
+          <Route exact path="/" component={TransactionList} />
+          <Route path="/overview" component={Overview} />
+        </div>
+      </Router>
     );
   }
 }
