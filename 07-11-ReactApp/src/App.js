a6eaaf015f6e59318b2a69762f92d8264f04e74a
Martina Kubištová <martina.kubistova@centrum.cz>
2018-05-03 19:06:13 +0200

    app

--- 07-11-ReactApp/src/App.js
+++ 07-11-ReactApp/src/App.js
@@ -4,28 +4,38 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import { Provider} from 'react-redux';
+import { createStore} from 'redux';
 import TransactionList from './components/TransactionList';
 import Overview from './components/Overview';
+import styled from 'styled-components';
+import transaction from './reducers/transaction';
+
+const store = createStore(transaction, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+//const StyleLink = styled(Link)``;
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="page">
-          <header className="App-header">
-            <h1>Welcome to React</h1>
-          </header>
-          <p>
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>
-            <Link to="/">Home</Link>
-            <Link to="/overview">Overview</Link>
-          </p>
-          <Route  path="/" component={TransactionList}/>
-          <Route  path="/overview" component={Overview}/>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="page">
+            <header className="App-header">
+              <h1>Welcome to React</h1>
+            </header>
+            <p>
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+            <p>
+              <Link to="/">To homepage</Link>
+              <Link to="/overview">To overview</Link>
+            </p>
+            <Route exact path="/" component={TransactionList} />
+            <Route path="/overview" component={Overview} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- 07-11-ReactApp/src/components/Overview.js
+++ 07-11-ReactApp/src/components/Overview.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default () => (
-    <div>ffaf</div>
+  <div>asdff</div>
 )
