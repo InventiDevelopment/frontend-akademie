9d9d37985c4ff26fde03f8cd78a866c30e824401
Aneta Czernekova <anczernek@gmail.com>
2018-05-15 15:26:31 +0200

    zmeny

--- ReactPrototypeInventi/src/components/Content.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.section`
-  padding: 0 20px;
-  margin-top: 70px;
-  margin-bottom: 80px;
-  overflow-y: auto;
-  height: ${ window.outerHeight - 150 }px
-`;
--- ReactPrototypeInventi/src/components/StyledIcon.js
+++
@@ -1,9 +0,0 @@
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import styled from 'styled-components';
-
-const StyledIcon = styled(FontAwesomeIcon)`
-  color: ${({ color }) => color};
-  margin: ${({ margin }) => margin};
-`;
-
-export default StyledIcon;
